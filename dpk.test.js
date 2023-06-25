563d505918fb0fa01738e35222a8e3660559bfb0
José Victor D. Fernandes <josevictordf@gmail.com>
2023-06-25 14:04:20 -0300

    test: remove unnecessary consts

--- dpk.test.js
+++ dpk.test.js
@@ -1,4 +1,8 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
+
+const HASH_ALGORITHM = 'sha3-512';
+const HASH_ENCODING = 'hex';
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
@@ -9,5 +13,30 @@ describe("deterministicPartitionKey", () => {
     const event = { partitionKey: 'valid-partition-key' };
     const trivialKey = deterministicPartitionKey(event);
     expect(trivialKey).toBe(event.partitionKey);
+  });
+  it("Returns the hash of the event partition key when it's bigger than MAX_PARTITION_KEY_LENGTH", () => {
+    const event = { partitionKey: 'valid-partition-key'.repeat(20) };
+    const eventHash = crypto
+      .createHash(HASH_ALGORITHM)
+      .update(event.partitionKey)
+      .digest(HASH_ENCODING);
+                      
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe(eventHash);
+  });
+  it("Returns the hash of the event when no partitionKey is provided", () => {
+    const event = 'valid-event-key';
+    const eventHash = crypto
+      .createHash(HASH_ALGORITHM)
+      .update(JSON.stringify(event))
+      .digest(HASH_ENCODING);
+                      
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe(eventHash);
+  });
+  it("Returns the event partition key as String when it isn't a String", () => {
+    const event = { partitionKey: 123456 };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe(JSON.stringify(event.partitionKey));
   });
 });
